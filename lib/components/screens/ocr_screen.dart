33a67c437ec3303d228dc9fc56656f3002c84616
Felipe Mattes <mattesfelipe@gmail.com>
2022-12-11 19:24:56 -0300

    folder structure changed to components/screen|widgets/

--- lib/components/screens/ocr_screen.dart
+++ lib/components/screens/ocr_screen.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile_vision_2/flutter_mobile_vision_2.dart';
 
-import 'ocr_text_detail.dart';
+import '../widgets/ocr_text_detail.dart';
 
 ///
 ///
